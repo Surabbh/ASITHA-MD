d752ee847c7890a1374e96310848d315a2cd8465
Surabbh <brosura01@gmail.com>
2024-10-24 21:08:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2yxzFbIQ#NSKdXspx8khp-bBfSs-BkZa6hJQzpoGHb49BO4t82kA",
-MONGODB: process.env.MONGODB || "https://downloads.mongodb.com/compass/mongodb-mongosh_2.0.0_amd64.deb",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+MONGODB: process.env.MONGODB || "mongodb+srv://ponnapkaya1:CUfG1C4P0RUfFsSk@cluster0.7qsf0.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };

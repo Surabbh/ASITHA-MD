8993cdfc3da8c93d95366b64fa47ae0488f66e6b
Surabbh <brosura01@gmail.com>
2024-10-24 21:53:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2yxzFbIQ#NSKdXspx8khp-bBfSs-BkZa6hJQzpoGHb49BO4t82kA",
-MONGODB: process.env.MONGODB || "mongodb+srv://brosura01:VVawIe1x450BJrUv@cluster0.39ggn.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+MONGODB: process.env.MONGODB || "mongodb+srv://brosura01:VVawIe1x450BJrUv@cluster0.39ggn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
